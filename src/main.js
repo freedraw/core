591f4a9b4d937223c22139e88bc18aa82a09a707
Nathan Dinsmore <nfdins@gmail.com>
2015-03-22 18:04:36 -0400

    Set hooks in main module

--- src/main.js
+++ src/main.js
@@ -4,4 +4,7 @@ css.load('theme/default/main.css')
 var Editor = require('editor')
 exports = new Editor().start()
 
+Hooks.getData = exports.getData.bind(exports)
+Hooks.loadData = exports.loadData.bind(exports)
+
 Native.done()
