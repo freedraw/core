367f64664fd42cc655ee7c122a180a865b7a9e72
Nathan Dinsmore <nfdins@gmail.com>
2015-03-22 19:57:35 -0400

    Add mock inspector UI

--- src/editor.js
+++ src/editor.js
@@ -2,15 +2,58 @@ var h = require('html')
 var Panel = require('panel')
 
 function Editor() {
-  this.layersPanel = new Panel(this, 215, h('.layers-panel', [
+  this.layersPanel = new Panel(this, 215, h('.layers panel', [
     h('.panel-header')
   ]))
 
   this.el = h('.editor', [
     this.layersPanel.el,
     h('.canvas'),
-    h('.inspector-panel', [
-      h('.panel-header')
+    h('.inspector panel', [
+      h('.panel-header'),
+      h('.panel-content', [
+        h('section.matrix', [
+          h('.row', [
+            h('label', ['Position']),
+            h('.field', [
+              h('input'),
+              h('label', ['X'])
+            ]),
+            h('.mid'),
+            h('.field', [
+              h('input'),
+              h('label', ['Y'])
+            ])
+          ]),
+          h('.row', [
+            h('label', ['Size']),
+            h('.field', [
+              h('input'),
+              h('label', ['Width'])
+            ]),
+            h('.mid'),
+            h('.field', [
+              h('input'),
+              h('label', ['Height'])
+            ])
+          ]),
+          h('.row', [
+            h('label', ['Transform']),
+            h('.field', [
+              h('input'),
+              h('label', ['Rotate'])
+            ]),
+            h('.mid'),
+            h('.field', [
+              h('.input', [
+                h('button'),
+                h('button')
+              ]),
+              h('label', ['Flip'])
+            ])
+          ])
+        ])
+      ])
     ])
   ])
 }
