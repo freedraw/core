2d38886ce69d4bb969cda7d712adf9326f7f3cc7
Nathan Dinsmore <nfdins@gmail.com>
2015-03-22 22:54:57 -0400

    Move inspector mock into mock-inspector module

--- src/editor.js
+++ src/editor.js
@@ -1,109 +1,20 @@
 var h = require('html')
 var Panel = require('panel')
+var MockInspector = require('mock-inspector')
 
 function Editor() {
-  this.layersPanel = new Panel(215, h('.layers panel', [
+  this.layers = new Panel(215, h('.layers panel', [
     h('.panel-header'),
     h('.panel-content'),
     h('.panel-footer')
   ]))
 
+  this.inspector = new MockInspector
+
   this.el = h('.editor', [
-    this.layersPanel.el,
+    this.layers.el,
     h('.canvas'),
-    h('.inspector panel', [
-      h('.panel-header'),
-      h('.panel-content', [
-        h('section.matrix', [
-          h('.row', [
-            h('label', ['Position']),
-            h('.field', [
-              h('input', {type: 'number', readOnly: true}),
-              h('label', ['X'])
-            ]),
-            h('.mid'),
-            h('.field', [
-              h('input', {type: 'number', readOnly: true}),
-              h('label', ['Y'])
-            ])
-          ]),
-          h('.row', [
-            h('label', ['Size']),
-            h('.field', [
-              h('input', {type: 'number', readOnly: true}),
-              h('label', ['Width'])
-            ]),
-            h('.mid'),
-            h('.field', [
-              h('input', {type: 'number', readOnly: true}),
-              h('label', ['Height'])
-            ])
-          ]),
-          h('.row', [
-            h('label', ['Transform']),
-            h('.field', [
-              h('input', {type: 'number', readOnly: true}),
-              h('label', ['Rotate'])
-            ]),
-            h('.mid'),
-            h('.field', [
-              h('.input', [
-                h('.segmented', [
-                  h('button.icon16', {disabled: true}),
-                  h('button.icon16', {disabled: true})
-                ])
-              ]),
-              h('label', ['Flip'])
-            ])
-          ])
-        ]),
-        h('section.matrix', [
-          h('.row', [
-            h('label', ['Opacity']),
-            h('.slider', [
-              h('input', {type: 'range', disabled: true})
-            ]),
-            h('.field', [
-              h('input', {type: 'number', readOnly: true})
-            ])
-          ]),
-          h('.row', [
-            h('label', ['Blending']),
-            h('.field', [
-              h('select', {disabled: true}, [
-                h('option', ['Normal']),
-                h('hr'),
-                h('option', ['Darken']),
-                h('option', ['Multiply']),
-                h('option', ['Color Burn']),
-                h('hr'),
-                h('option', ['Lighten']),
-                h('option', ['Screen']),
-                h('option', ['Color Dodge']),
-                h('hr'),
-                h('option', ['Overlay']),
-                h('option', ['Soft Light']),
-                h('option', ['Hard Light']),
-                h('hr'),
-                h('option', ['Difference']),
-                h('option', ['Exclusion']),
-                h('hr'),
-                h('option', ['Hue']),
-                h('option', ['Saturation']),
-                h('option', ['Color']),
-                h('option', ['Luminosity'])
-              ])
-            ])
-          ])
-        ]),
-        h('section', [h('h1', ['Fills'])]),
-        h('section', [h('h1', ['Borders'])]),
-        h('section', [h('h1', ['Shadows'])]),
-        h('section', [h('h1', ['Inner Shadows'])]),
-        h('section', [h('h1', ['Gaussian Blur'])]),
-      ]),
-      h('.panel-footer')
-    ])
+    this.inspector.el
   ])
 }
 
---
+++ src/mock-inspector.js
@@ -0,0 +1,99 @@
+var h = require('html')
+
+function MockInspector() {
+  this.el = h('.inspector panel', [
+    h('.panel-header'),
+    h('.panel-content', [
+      h('section.matrix', [
+        h('.row', [
+          h('label', ['Position']),
+          h('.field', [
+            h('input', {type: 'number', readOnly: true}),
+            h('label', ['X'])
+          ]),
+          h('.mid'),
+          h('.field', [
+            h('input', {type: 'number', readOnly: true}),
+            h('label', ['Y'])
+          ])
+        ]),
+        h('.row', [
+          h('label', ['Size']),
+          h('.field', [
+            h('input', {type: 'number', readOnly: true}),
+            h('label', ['Width'])
+          ]),
+          h('.mid'),
+          h('.field', [
+            h('input', {type: 'number', readOnly: true}),
+            h('label', ['Height'])
+          ])
+        ]),
+        h('.row', [
+          h('label', ['Transform']),
+          h('.field', [
+            h('input', {type: 'number', readOnly: true}),
+            h('label', ['Rotate'])
+          ]),
+          h('.mid'),
+          h('.field', [
+            h('.input', [
+              h('.segmented', [
+                h('button.icon16', {disabled: true}),
+                h('button.icon16', {disabled: true})
+              ])
+            ]),
+            h('label', ['Flip'])
+          ])
+        ])
+      ]),
+      h('section.matrix', [
+        h('.row', [
+          h('label', ['Opacity']),
+          h('.slider', [
+            h('input', {type: 'range', disabled: true})
+          ]),
+          h('.field', [
+            h('input', {type: 'number', readOnly: true})
+          ])
+        ]),
+        h('.row', [
+          h('label', ['Blending']),
+          h('.field', [
+            h('select', {disabled: true}, [
+              h('option', ['Normal']),
+              h('hr'),
+              h('option', ['Darken']),
+              h('option', ['Multiply']),
+              h('option', ['Color Burn']),
+              h('hr'),
+              h('option', ['Lighten']),
+              h('option', ['Screen']),
+              h('option', ['Color Dodge']),
+              h('hr'),
+              h('option', ['Overlay']),
+              h('option', ['Soft Light']),
+              h('option', ['Hard Light']),
+              h('hr'),
+              h('option', ['Difference']),
+              h('option', ['Exclusion']),
+              h('hr'),
+              h('option', ['Hue']),
+              h('option', ['Saturation']),
+              h('option', ['Color']),
+              h('option', ['Luminosity'])
+            ])
+          ])
+        ])
+      ]),
+      h('section', [h('h1', ['Fills'])]),
+      h('section', [h('h1', ['Borders'])]),
+      h('section', [h('h1', ['Shadows'])]),
+      h('section', [h('h1', ['Inner Shadows'])]),
+      h('section', [h('h1', ['Gaussian Blur'])]),
+    ]),
+    h('.panel-footer')
+  ])
+}
+
+exports = MockInspector
