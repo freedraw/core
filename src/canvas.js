bf6f41ff37350ed62da518d8261bbe3fa9a502d3
Nathan Dinsmore <nfdins@gmail.com>
2015-03-23 00:39:31 -0400

    Add Canvas centerX/Y and scale

--- src/canvas.js
+++ src/canvas.js
@@ -3,7 +3,7 @@ var svg = require('svg')
 
 function Canvas() {
   this.el = h('.canvas', [
-    svg('svg', [
+    this.svg = svg('svg', [
       svg('circle', {cx: 50, cy: 50, r: 30, style: {
         fill: 'red',
         stroke: 'black',
@@ -11,6 +11,22 @@ function Canvas() {
       }})
     ])
   ])
+
+  this.centerX = 0
+  this.centerY = 0
+  this.scale = 1
+}
+
+Canvas.prototype.updateViewBox = function() {
+  var box = this.svg.viewBox.baseVal
+  var viewport = this.el.getBoundingClientRect()
+  var width = this.scale * viewport.width
+  var height = this.scale * viewport.height
+
+  box.x = this.centerX - width / 2
+  box.y = this.centerY - height / 2
+  box.width = width
+  box.height = height
 }
 
 exports = Canvas
