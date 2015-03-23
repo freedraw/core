51484430ddf1574079eb3612b6c73f30bf76316c
Nathan Dinsmore <nfdins@gmail.com>
2015-03-23 13:49:29 -0400

    Highlight hovered SVG node

--- src/canvas.js
+++ src/canvas.js
@@ -1,5 +1,6 @@
 var h = require('html')
 var svg = require('svg')
+var Vec2 = require('vec2')
 var commands = require('commands')
 var convertWheelUnits = require('convert-wheel-units')
 
@@ -15,6 +16,18 @@ function Canvas() {
         fill: 'red',
         stroke: 'black',
         strokeWidth: '2px'
+      }}),
+      svg('circle', {cx: 50, cy: 40, r: 60, style: {
+        fill: 'yellow',
+        stroke: 'black',
+        strokeWidth: '1px'
+      }})
+    ]),
+    this.ui = svg('svg', {style: {pointerEvents: 'none'}}, [
+      this.highlightPath = svg('path', {style: {
+        fill: 'none',
+        stroke: 'rgb(68, 192, 255)',
+        strokeWidth: '2px'
       }})
     ])
   ])
@@ -23,6 +36,11 @@ function Canvas() {
   this.centerY = 0
   this.scale = 1
 
+  this.mouseX = NaN
+  this.mouseY = NaN
+  this.highlightedObject = null
+
+  this.el.addEventListener('mousemove', this.onMouseMove.bind(this))
   this.el.addEventListener('magnify', this.onMagnify.bind(this))
   this.el.addEventListener('gestureend', this.constrainZoom.bind(this))
   this.el.addEventListener('wheel', this.onWheel.bind(this))
@@ -31,6 +49,12 @@ function Canvas() {
   commands.on('zoomCenter', this.zoomCenter, this)
   commands.on('zoomIn', this.zoomBy.bind(this, ZOOM_FACTOR - 1))
   commands.on('zoomOut', this.zoomBy.bind(this, 1 / ZOOM_FACTOR - 1))
+}
+
+Canvas.prototype.onMouseMove = function(e) {
+  this.mouseX = e.clientX
+  this.mouseY = e.clientY
+  this.highlightObject(e.target === this.svg ? null : e.target)
 }
 
 Canvas.prototype.onWheel = function(e) {
@@ -97,6 +121,42 @@ Canvas.prototype.updateViewBox = function() {
   box.y = this.centerY - height / 2
   box.width = width
   box.height = height
+
+  if (this.mouseX === this.mouseX) {
+    var t = document.elementFromPoint(this.mouseX, this.mouseY)
+    this.highlightObject(t === this.svg ? null : t)
+  }
+}
+
+Canvas.prototype.highlightObject = function(object) {
+  this.highlightedObject = object
+
+  var path = this.highlightPath
+  var list = path.pathSegList
+  list.clear()
+
+  if (!object) return
+  var ctm = object.getCTM()
+
+  switch (object.localName) {
+    case 'circle':
+      var r = object.r.baseVal.value
+      var cx = object.cx.baseVal.value
+      var cy = object.cy.baseVal.value
+
+      var major = new Vec2(cx + r, cy).transform(ctm)
+      var major2 = new Vec2(cx - r, cy).transform(ctm)
+      var minor = new Vec2(cx, cy + r).transform(ctm)
+      var minor2 = new Vec2(cx, cy - r).transform(ctm)
+
+      var rx = major.sub(major2).length() / 2
+      var ry = minor.sub(minor2).length() / 2
+
+      list.appendItem(path.createSVGPathSegMovetoAbs(major.x, major.y))
+      list.appendItem(path.createSVGPathSegArcAbs(major2.x, major2.y, rx, ry, 0, true, false))
+      list.appendItem(path.createSVGPathSegArcAbs(major.x, major.y, rx, ry, 0, true, false))
+      break
+  }
 }
 
 exports = Canvas
