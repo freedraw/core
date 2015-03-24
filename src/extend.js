f15ad31c6172a7bd3396d9c76232fc2ac83a114f
Nathan Dinsmore <nfdins@gmail.com>
2015-03-24 16:46:29 -0400

    Add extend utility

---
+++ src/extend.js
@@ -0,0 +1,12 @@
+exports = function extend(o, p) {
+  for (var k in p) {
+    o[k] = p[k]
+  }
+  return o
+}
+
+exports.quiet = function(o, p) {
+  for (var k in p) if (!(k in o)) {
+    o[k] = p[k]
+  }
+}
