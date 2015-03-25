46ff310d738f109f1840085886f16a694db172f5
Nathan Dinsmore <nfdins@gmail.com>
2015-03-24 21:08:24 -0400

    Implement replaceBBox for rectangles

--- src/canvas.js
+++ src/canvas.js
@@ -260,7 +260,13 @@ Canvas.prototype.replaceBBox = function(object, rect) {
         object.rx.baseVal.value = rect.width / 2
         object.ry.baseVal.value = rect.height / 2
       }
-      break
+      return
+    case 'rect':
+      object.x.baseVal.value = rect.x
+      object.y.baseVal.value = rect.y
+      object.width.baseVal.value = rect.width
+      object.height.baseVal.value = rect.height
+      return
   }
 }
 
