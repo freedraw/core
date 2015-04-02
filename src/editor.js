02c8c9c715b0b9892b49788be6e45bd3b92ef7c8
Nathan Dinsmore <nfdins@gmail.com>
2015-04-02 08:24:53 -0400

    Remove a console.log

--- src/editor.js
+++ src/editor.js
@@ -36,7 +36,6 @@ Editor.prototype.getData = function(type) {
 }
 
 Editor.prototype.loadData = function(type, data) {
-  console.log(type, data)
   if (type !== 'public.svg-image') throw new Error("Unimplemented")
   var document = new DOMParser().parseFromString(data, 'image/svg+xml')
   this.canvas.setDocument(document)
